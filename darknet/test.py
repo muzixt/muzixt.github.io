12fa1377c8f76521fcb037433196bc0795639a01
muzixt <97214808+muzixt@users.noreply.github.com>
2022-03-16 20:10:30 +0800

    Update test.py

--- darknet/test.py
+++ darknet/test.py
@@ -11,15 +11,15 @@ import torchvision.datasets as datasets
 import numpy as np
 from model import darknet53
 from net2 import *
-from train import evaluate, predict
+from train import hisEqulColorTransform, predict, evaluate, accuracy
 
 #
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 transform_test = transforms.Compose([
+    # hisEqulColorTransform(1),
     transforms.Resize((224, 224)),
-    # transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225]),
@@ -27,19 +27,42 @@ transform_test = transforms.Compose([
 ds_test = datasets.ImageFolder("./data/test/",
                                transform=transform_test, target_transform=lambda t: torch.tensor([t]).float())
 
-testLoader = DataLoader(ds_test, batch_size=32, shuffle=False, num_workers=8)
+batch_size = 32
+testLoader = DataLoader(ds_test, batch_size=batch_size, shuffle=False, num_workers=8)
+
+# Loss = nn.CrossEntropyLoss()
 
 class_to_idx = ds_test.class_to_idx
 
 class_to_nums = {k: ds_test.targets.count(v) for k, v in class_to_idx.items()}
 #
-# print(class_to_idx)
-# print(class_to_nums)
+print(class_to_idx)
+print(class_to_nums)
 
 model = resnet34(num_classes=5).to(device)
 
-state = torch.load("./log/model/Best_2022-03-12-19-57-00-116956_model.pth")
+# results = evaluate(model, testLoader, loss_func=Loss)
+
+
+state = torch.load("./log/model/Best_2022-03-16-11-27-50-672051_model.pth")
 model.load_state_dict(state['state_dict'], strict=False)
+
+
+@torch.no_grad()
+def predict(model, dl):
+    model.eval()
+    pp = []
+    for i, t in enumerate(dl):
+        p = model(t[0].to(device))
+        # acc = accuracy(p, t[1].to(device))
+        # print(acc)
+        pp.append(p)
+        print("\r{}/{}".format(i + 1, len(ds_test) // batch_size), end="")
+    print()
+    result = torch.cat(pp)
+    target = torch.cat([t[1] for t in dl])
+    return result.data, target.data
+
 
 pre, target = predict(model, testLoader)
 _, pre = pre.topk(1, 1)
